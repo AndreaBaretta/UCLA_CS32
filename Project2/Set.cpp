603e2f1b2db57a3090bd593626d0842f936db88f
Andrea Baretta <andrea.m.baretta@gmail.com>
2023-01-30 22:23:04 -0800

    [Project2] Add comments

--- Project2/Set.cpp
+++ Project2/Set.cpp
@@ -2,8 +2,15 @@
 #include <cassert>
 #include <iostream>
 
+/**
+ * Default constructor.
+*/
 Set::Set() : m_head(nullptr), m_size(0) {} // Default constructor
 
+
+/**
+ * Copy constructor. Takes a const reference and copies dynamically allocated memory.
+*/
 Set::Set(const Set& other) : m_head(nullptr), m_size(other.m_size) {
     if (m_size == 0) { return; } // Edge case: size of other is 0
     NodeType* p = nullptr;              // The pointer belonging to this object
@@ -18,6 +25,10 @@ Set::Set(const Set& other) : m_head(nullptr), m_size(other.m_size) {
     }
 }
 
+
+/**
+ * Destructor. Frees memory of all nodes.
+*/
 Set::~Set() {
     if (empty()) { return; } // Edge case: set is empty
     NodeType* p = m_head;
@@ -28,6 +39,9 @@ Set::~Set() {
     delete p; // Last node to be deleted: the tail
 }
 
+/**
+ * Copy assignment operator. Uses copy constructor to copy rhs.
+*/
 Set& Set::operator=(const Set& rhs) {
     Set tmp = Set(rhs); // Create copy of rhs
     this->swap(tmp); // Use swap to handle switching with rhs
@@ -35,25 +49,36 @@ Set& Set::operator=(const Set& rhs) {
     // As soon as tmp goes out of scope, destructor gets called, cleaning up memory that used to belong to *this
 }
 
+
+/**
+ * Returns if set is empty
+*/
 bool Set::empty() const {
     return m_size == 0;
 }
 
+/**
+ * Returns size of set.
+*/
 int Set::size() const {
     return m_size;
 }
 
+/**
+ * Inserts new items into linked list. If value already exists, returns false. If value does not, then it is copied into a new node in the linked list.
+ * Items are ordered from least to greatest
+*/
 bool Set::insert(const ItemType& value) {
-    if (empty()) { // Edge case
+    if (empty()) { // Edge case: set is empty
         m_head = new NodeType{.item=ItemType(value), .next=nullptr, .prev=nullptr};
         ++m_size;
         return true;
     }
     NodeType* p = m_head;
-    while (p != nullptr) {
+    while (p != nullptr) { // If set is not empty, continue iterating until exit conditions are met
         if (p->item == value) { return false; } // Item already exists
 
-        if (value < p->item) {
+        if (value < p->item) { // True if we find slot into which to place the new node. It will go the left of p
 
             NodeType* new_node = new NodeType{.item=ItemType(value), .next=p, .prev=p->prev};
             if (p->prev != nullptr) { // Inserting at head
@@ -70,57 +95,66 @@ bool Set::insert(const ItemType& value) {
             return true;
         }
         if (p->next == nullptr) { // Inserting to tail
-            NodeType* new_node = new NodeType{.item=ItemType(value), .next=nullptr, .prev=p};
+            NodeType* new_node = new NodeType{.item=ItemType(value), .next=nullptr, .prev=p}; // Insert to the right of the old tail
             p->next = new_node;
             ++m_size;
             return true;
         }
         p = p->next;
     }
-    assert(false); // Probably shouldn't put this in file code
-                   // But you only live once, ya know?
-    return false;
+    return false; // SHOULD NEVER BE CALLED!!!! All possible outcomes should have been dealt with already:
+                  // 1. Not inserting value
+                  // 2. Inserting at head
+                  // 3. Inserting in the middle
+                  // 4. Inserting at tail
+
 }
 
+/**
+ * Removes Node matching value from the list, and free corresponding memory. Returns true if successful, false if value does not exist in set
+*/
 bool Set::erase(const ItemType& value) {
-    if (empty()) { return false; }
+    if (empty()) { return false; } // Edge case: set is empty
     NodeType* p = m_head;
-    if (p->item == value) { // Deleting head
-        if (p->next != nullptr) {
-            p->next->prev = nullptr;
+    if (p->item == value) { // Edge case: Deleting head
+        if (p->next != nullptr) { // If there is a second element...
+            p->next->prev = nullptr; // ... make sure it doesn't point to the ex-head
         }
-        m_head = p->next;
-        delete p;
+        m_head = p->next; // Change head to new, actual head
+        delete p; // Free corresponding memory
         --m_size;
         return true;
     }
-    while (p->next != nullptr) { // Normal case: Malcolm's in the middle
-        if (p->item == value) {
-            p->prev->next = p->next;
-            p->next->prev = p->prev;
-            delete p;
+    while (p->next != nullptr) { // Normal case: Item is in the middle
+        if (p->item == value) { // If p is identified to be deleted
+            p->prev->next = p->next; // p's previous element must point to p's next element
+            p->next->prev = p->prev; // vice versa: next element must point to p's previous element
+            delete p; // Free corresponding memory
             --m_size;
             return true;
         }
         p = p->next;
     }
-    if (p->item == value) { // Deleting tail
-        p->prev->next = nullptr;
-        delete p;
+    if (p->item == value) { // Edge case: Deleting tail
+        p->prev->next = nullptr; // For this case to be triggered, there must be at least two elements, so it's safe to make use of the element before p
+        delete p; // Free corresponding memory
         --m_size;
         return true;
     }
-    return false;
+    return false; // If nothing triggered: no node contains the given value, so fail
 }
 
+/**
+ * Returns true if set contains node with value, false if not
+*/
 bool Set::contains(const ItemType& value) const {
-    if (empty()) { return false; }
+    if (empty()) { return false; } // Edge case: set is empty
     NodeType* p = m_head;
-    while (p->next != nullptr) {
-        if (p->item == value) { return true; }
+    while (p->next != nullptr) { // Iterate through until we reach the tail
+        if (p->item == value) { return true; } // If we've found value: return true
         p = p->next;
     }
-    return p->item == value;
+    return p->item == value; // The last possible element that could contain value is
 }
 
 bool Set::get(int pos, ItemType& value) const {
