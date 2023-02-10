90b7cb455a398a5679542ae45288fd550421c2b3
Andrea Baretta <andrea.m.baretta@gmail.com>
2023-02-10 12:09:19 -0800

    [Homework3] Solve problem 3

---
+++ Homework3/maze.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Coord {
+   public:
+    Coord(int r, int c) : m_row(r), m_col(c) {}
+    int r() const { return m_row; }
+    int c() const { return m_col; }
+
+   private:
+    int m_row;
+    int m_col;
+};
+
+// Return true if there is a path from (sr,sc) to (er,ec)
+// through the maze; return false otherwise
+bool pathExists(string maze[],
+                int nRows,
+                int nCols,
+                int sr,
+                int sc,
+                int er,
+                int ec) {
+    if (sr == er && sc == ec) { return true; }
+    maze[sr][sc] = '0';
+
+    // cerr << "Explored coord: (" << r << "," << c << ")" << endl;
+    if (maze[sr][sc+1] != '0' && maze[sr][sc+1] != 'X') { if (pathExists(maze, nRows, nCols, sr, sc+1, er, ec)) { return true; } }
+    if (maze[sr-1][sc] != '0' && maze[sr-1][sc] != 'X') { if (pathExists(maze, nRows, nCols, sr-1, sc, er, ec)) { return true; } }
+    if (maze[sr][sc-1] != '0' && maze[sr][sc-1] != 'X') { if (pathExists(maze, nRows, nCols, sr, sc-1, er, ec)) { return true; } }
+    if (maze[sr+1][sc] != '0' && maze[sr+1][sc] != 'X') { if (pathExists(maze, nRows, nCols, sr+1, sc, er, ec)) { return true; } }
+
+    return false;
+}
+
+// If the start location is equal to the ending location, then we've
+//     solved the maze, so return true.
+// Mark the start location as visted.
+// For each of the four directions,
+//     If the location one step in that direction (from the start
+//         location) is unvisited,
+//             then call pathExists starting from that location (and
+//                         ending at the same ending location as in the
+//                         current call).
+//                     If that returned true,
+//                         then return true.
+// Return false.
+
+int main() {
+    string maze[10] = {
+        "XXXXXXXXXX",
+        "X..X...X.X",
+        "X.XXXX.X.X",
+        "X.X.X..X.X",
+        "X...X.XX.X",
+        "XXX......X",
+        "X.X.XXXX.X",
+        "X.XXX....X",
+        "X...X..X.X",
+        "XXXXXXXXXX"
+    };
+
+    if (pathExists(maze, 10,10, 5,3, 8,8)) {
+        cerr << "Solvable!" << endl;
+    } else {
+        cerr << "Out of luck!" << endl;
+    }
+}
